a08d6c56383f4fb65def4a361fd718642b144127
alamin islam <alamin.ndc.17@gmail.com>
2025-12-04 14:45:47 +0600

    created all crud operation for secound table todos and add notes

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ const initDB = async () => {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS todos(
       id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,           /* Here the thing happen */
       title VARCHAR(200) NOT NULL,
       description TEXT,
       completed BOOLEAN DEFAULT false,
@@ -207,7 +207,90 @@ app.get("/todos", async (req: Request, res: Response) => {
       details: err,
     });
   }
-});  
+});
+
+app.get("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos WHERE id= $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "todos data not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "user fatched successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.put("/todos/:id", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE todos SET user_id=$1, title=$2 WHERE id=$3 RETURNING *`,
+      [user_id, title, req.params.id]
+    );
+    // console.log(result.rows);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "todos data not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "todos data update successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.delete("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM todos WHERE id= $1`, [
+      req.params.id,
+    ]);
+
+    // console.log(result.rows);
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "todos data not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "todos data deleted successfully",
+        data: null,
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+/* Summary : Here todos table is refference table for User . Its means User is mother and todos is child. We can delete child(todos) and there will be not impect on mother (Users). But if we delete mother(Users) child will be deleted. Other things is WE can create a lot child by reffering a single mother's id. Everyhting will be idetified by id   */
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
