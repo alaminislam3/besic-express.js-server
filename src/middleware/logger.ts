41a15675a90a6e814d8cc8b20b1495d730cd61ff
alamin islam <alamin.ndc.17@gmail.com>
2025-12-14 23:45:59 +0600

    practicing moduler pattern

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-// logger middleware
+// logger middleware new
 const logger = (req: Request, res: Response, next: NextFunction) => {
   // console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
   next();
