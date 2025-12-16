7203834bb79565a8e9edb9b0a99f7c6f0a5d57b2
alamin islam <alamin.ndc.17@gmail.com>
2025-12-16 23:44:08 +0600

    deletd some imported

--- src/middleware/modules/user/user.controller.ts
+++ src/middleware/modules/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
-import { pool } from "../../../config/db";
 import { userService } from "./user.service";
-import { Result } from "pg";
+
 
 const createUser = async (req: Request, res: Response) => {
   try {
